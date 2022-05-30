be55de21d3fc278818380fbfdb49298139d0368f
Sumit <sumitsp198@gmail.com>
2022-05-30 07:55:42 +0530

    ..

---
+++ firstapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Os from "./components/mobileos";
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Os />
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ firstapp/src/components/mobileos.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Os = () => {
+    return (
+        <div>
+            <h1>Mobile Operating System</h1>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>Iphone</li>
+            <li>Windows Phone</li>
+            <h1>Mobile Manufactures</h1>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </div>
+        
+        
+        
+    )
+};
+
+export default Os;
